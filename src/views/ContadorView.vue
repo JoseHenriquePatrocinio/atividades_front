c94b1abff6ceb891e1bae30440f6a6a950c6132f
JoseHenriquePatrocinio <henriquepatrocinio98@gmail.com>
2024-02-21 21:39:20 -0300

    feat: estrutura html e font-awesome

---
+++ src/views/ContadorView.vue
@@ -0,0 +1,23 @@
+<template>
+  <body>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
+
+    <div class="counter-container">
+      <i class="fab fa-twitter fa-3x"></i>
+      <div class="counter" :data-target="12000"></div>
+      <span>Seguidores do Twitter</span>
+    </div>
+
+    <div class="counter-container">
+      <i class="fab fa-youtube fa-3x"></i>
+      <div class="counter" :data-target="5000"></div>
+      <span>Assinantes do YouTube</span>
+    </div>
+
+    <div class="counter-container">
+      <i class="fab fa-facebook fa-3x"></i>
+      <div class="counter" :data-target="7500"></div>
+      <span>Fãs do Facebook</span>
+    </div>
+  </body>
+</template>
