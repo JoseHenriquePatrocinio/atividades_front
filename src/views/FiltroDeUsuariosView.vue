a2df02de3efdfdc9ed062d98a1a44634bee55e03
JoseHenriquePatrocinio <henriquepatrocinio98@gmail.com>
2024-02-24 02:34:15 -0300

    feat: adicionado estilo

--- src/views/FiltroDeUsuariosView.vue
+++ src/views/FiltroDeUsuariosView.vue
@@ -29,19 +29,103 @@ export default {
 </script>
 
 <style scoped>
+.container {
+    border-radius: 5px;
+    box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.2);
+    overflow: hidden;
+    width: 300px;
+    margin-left: auto;
+    margin-right: auto;
+}
+
+.title {
+    margin: 0;
+}
+
+.subtitle {
+    display: inline-block;
+    margin: 5px 0 20px;
+    opacity: 0.8;
+}
+
+.header {
+    background-color: #626367;
+    color: #fff;
+    padding: 30px 20px;
+    margin: 0 auto;
+}
+
+.header input {
+    background-color: rgba(0, 0, 0, 0.3);
+    border: 0;
+    border-radius: 50px;
+    color: #fff;
+    font-size: 14px;
+    padding: 10px 05px;
+    width: 100%;
+    margin: 0 auto;
+}
+
+.header input:focus {
+    outline: none;
+}
+
+.user-list {
+    background-color: #fff;
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    max-height: 400px;
+    overflow-y: auto;
+}
+
+.user-list li {
+    display: flex;
+    padding: 20px;
+}
+
+.user-list img {
+    border-radius: 50%;
+    object-fit: cover;
+    height: 50px;
+    width: 50px;
+}
+
+.user-list .user-info {
+    margin-left: 10px;
+}
+
+.user-list .user-info h4 {
+    margin: 0 0 10px;
+}
+
+.user-list .user-info p {
+    font-size: 12px;
+}
+
+.user-list li:not(:last-of-type) {
+    border-bottom: 1px solid #eee;
+}
+
+.user-list li.hide {
+    display: none;
+}
+
 .dadosDownload {
-  text-align: center;
-  margin-top: 20px;
+    text-align: center;
+    margin-top: 20px;
 }
+
 .dadosDownload a {
-  text-decoration: none;
-  color: #333;
-  font-weight: bold;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+    text-decoration: none;
+    color: #333;
+    font-weight: bold;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 }
+
 .dadosDownload a i {
-  margin-right: 5px;
+    margin-right: 5px;
 }
 </style>
