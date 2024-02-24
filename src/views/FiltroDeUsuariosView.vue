998bc15d8c6234f44f4abb6eb951d2d18f68aaf6
JoseHenriquePatrocinio <henriquepatrocinio98@gmail.com>
2024-02-24 01:59:48 -0300

    feat: estilizando botao de download

--- src/views/FiltroDeUsuariosView.vue
+++ src/views/FiltroDeUsuariosView.vue
@@ -1,5 +1,7 @@
 <template>
     <body>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
+
         <header class="header">
             <h4 class="title">Filtrar Usuário</h4>
             <small class="subtitle">Pesquise por nome e/ou localização</small>
@@ -25,3 +27,21 @@ export default {
     name: "FiltroDeUsuariosView",
 }
 </script>
+
+<style scoped>
+.dadosDownload {
+  text-align: center;
+  margin-top: 20px;
+}
+.dadosDownload a {
+  text-decoration: none;
+  color: #333;
+  font-weight: bold;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.dadosDownload a i {
+  margin-right: 5px;
+}
+</style>
