6f3a0388198d181d3a5350c8b7fcce44aeb39140
JoseHenriquePatrocinio <henriquepatrocinio98@gmail.com>
2024-02-23 23:26:07 -0300

    feat: implementando estrutura e rota

---
+++ src/views/CliqueParaCurtirView.vue
@@ -0,0 +1,10 @@
+<template>
+    <body>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
+
+        <h3>Clique duas vezes na imagem<i class="fas fa-heart"></i>para curtir.</h3>
+        <small>Você curtiu <span id="times">0</span> vezes</small>
+
+        <div class="loveMe"></div>
+    </body>
+</template>
