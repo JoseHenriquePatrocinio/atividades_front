007d6d176844a895acbc727416d5e78871277404
JoseHenriquePatrocinio <henriquepatrocinio98@gmail.com>
2024-02-23 21:12:25 -0300

    feat: preparando estrutura e rota

---
+++ src/views/FaqView.vue
@@ -0,0 +1,75 @@
+<template>
+    <body>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
+
+        <h1>Perguntas Frequentes</h1>
+        <div class="faq-container">
+            <div class="faq active">
+                <h3 class="faq-title">
+                    Qual é a capital do Brasil presente em todas as festas de aniversário?
+                </h3>
+
+                <p class="faq-text">
+                    Palmas.
+                </p>
+
+                <button class="faq-toggle">
+                    <i class="fas fa-chevron-down"></i>
+                    <i class="fas fa-times"></i>
+                </button>
+            </div>
+
+            <div class="faq">
+                <h3 class="faq-title">
+                    O que é pequeno em Lisboa e grande no Brasil?
+                </h3>
+                <p class="faq-text">
+                    A letra B (minúscula em Lisboa e maiúscula em Brasil).
+                </p>
+                <button class="faq-toggle">
+                    <i class="fas fa-chevron-down"></i>
+                    <i class="fas fa-times"></i>
+                </button>
+            </div>
+
+            <div class="faq">
+                <h3 class="faq-title">
+                    Qual é a maneira orientada a objetos de se tornar rico?
+                </h3>
+                <p class="faq-text">
+                    Herança.
+                </p>
+                <button class="faq-toggle">
+                    <i class="fas fa-chevron-down"></i>
+                    <i class="fas fa-times"></i>
+                </button>
+            </div>
+
+            <div class="faq">
+                <h3 class="faq-title">
+                    Qual é a operadora que concorda sempre?
+                </h3>
+                <p class="faq-text">
+                    Claro.
+                </p>
+                <button class="faq-toggle">
+                    <i class="fas fa-chevron-down"></i>
+                    <i class="fas fa-times"></i>
+                </button>
+            </div>
+
+            <div class="faq">
+                <h3 class="faq-title">
+                    O que o açaí disse para os filhos?
+                </h3>
+                <p class="faq-text">
+                    O último açaí fecha a porta.
+                </p>
+                <button class="faq-toggle">
+                    <i class="fas fa-chevron-down"></i>
+                    <i class="fas fa-times"></i>
+                </button>
+            </div>
+        </div>
+    </body>
+</template>
